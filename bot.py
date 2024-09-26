122c38f8cc08198b68e676f7a0a0ea12027aa2bb
juger1 <gajehaj8@gmail.com>
2024-09-26 14:33:33 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,14 +1,15 @@
-import os, asyncio
+import os
+import asyncio
 from configs import *
 from aiohttp import web
 from logger import logger
 from pyrogram import Client
 from utilities import web_server, ping_server
 
-class ShortnerBot(Client):
+class AdlinkfyShortnerBot(Client):
     def __init__(self):
         super().__init__(
-            "shortner_bot",
+            "AdlinkfyShortnerBot",
             api_id=API_ID,
             api_hash=API_HASH,
             bot_token=BOT_TOKEN,
@@ -23,13 +24,25 @@ class ShortnerBot(Client):
         port = int(os.getenv("PORT", 8080))
         await web.TCPSite(app, ba, port).start()
         await super().start()
-        logger.info("Bot started successfully - @Tamilan_botsz & @Sharathitsisme..")
+
+        # Send a message to the owner indicating the bot has started
+        try:
+            await self.send_message(chat_id=int(OWNER_ID), text="<b>Bot Started!</b>")
+        except Exception as err:
+            logger.error("Boot alert failed! Please start the bot in PM: %s", err)
+
+        logger.info("Bot started successfully, Developer @StupidBoi69")
         asyncio.create_task(ping_server())
 
     async def stop(self, *args):
-        await super().stop()
-        logger.info("Bot stopped - @Tamilan_botsz & @sharathitsisme!")
+        try:
+            await self.send_message(chat_id=int(OWNER_ID), text="<b>Bot Stopped!</b>")
+        except Exception as err:
+            logger.error("Stop alert failed! Please start the bot in PM: %s", err)
 
+        await super().stop()
+        logger.info("Bot stopped - Developer @StupidBoi69")
 
 if __name__ == '__main__':
-    ShortnerBot().run()
+    AdlinkfyShortnerBot().run()
+
