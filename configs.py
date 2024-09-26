b77a3ffd1922ea52779ff8f8cbb04fd8a85f53bf
juger1 <gajehaj8@gmail.com>
2024-09-26 17:55:37 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -3,7 +3,6 @@ from os import getenv as genv
 API_ID = genv("API_ID", "25695562")
 API_HASH = genv("API_HASH", "0b691c3e86603a7e34aae0b5927d725a")
 BOT_TOKEN = genv("BOT_TOKEN", "7083722060:AAEp1Ec33BYPaHiVNZsTZWoe3U251JtuXsA")
-OWNER_ID = int(genv("OWNER_ID", "1895952308"))
 BASE_URL = genv("BASE_URL", "runurl.in")
 SUPPORT_CHAT = genv("SUPPORT_CHAT", "runurl")  # Make sure this variable is defined
 UPDATE_CHANNEL = genv("UPDATE_CHANNEL", "run_url")  # Make sure this variable is defined
